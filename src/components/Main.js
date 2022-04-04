1452251fb6e6ccc576368539995ff5b42df6cf37
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-04 16:40:50 +0700

    posts2

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ function Main() {
         <main className="main">
             <img className="main__image" src={image} />
             <section className="profile">
-                <img className="profile__avatar"/>
+                <img className="profile__avatar" />
                 <h1 className="profile__name">Marina Z</h1>
                 <p className="profile__about">I like cats. I want to learn React, bacause I need to find a good job.</p>
             </section>
@@ -13,7 +13,11 @@ function Main() {
                 <h2 className="posts__title">My posts</h2>
                 <input className="posts__input" placeholder='your news...'></input>
                 <button className="posts__button">Send</button>
-                <p>Hey, nobody love me?</p>
+                <ul className="posts__item-list">
+                    <li className="posts__item">
+                        <span className="posts__text">Hey, nobody love me?</span>
+                    </li>
+                </ul>
             </section>
         </main>
     )
