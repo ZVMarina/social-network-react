6427c518a1f5a257e323d3413c900b211c429ce7
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-04 17:36:38 +0700

    picture profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,13 @@ function Main() {
         <main className="main">
             <img className="main__image" src={image} />
             <section className="profile">
-                <img className="profile__avatar" src="https://games.mail.ru/hotbox/content_files/news/2021/06/29/150b75b87bf64e07a2465f34aa08d7e7.jpg"/>
-                <h1 className="profile__name">Marina Z</h1>
-                <p className="profile__about">I like cats. I want to learn React, bacause I need to find a good job.</p>
+                <picture className="profile__picture">
+                    <img className="profile__avatar" src="https://games.mail.ru/hotbox/content_files/news/2021/06/29/150b75b87bf64e07a2465f34aa08d7e7.jpg" />
+                </picture>
+                <div className="profile__text">
+                    <h1 className="profile__name">Marina Z</h1>
+                    <p className="profile__about">I like cats. I want to learn React, bacause I need to find a good job.</p>
+                </div>
             </section>
             <section className="posts">
                 <h2 className="posts__title">My posts</h2>
