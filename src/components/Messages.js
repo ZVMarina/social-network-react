29d1c124f2e60493dbc1f29edd99ac8f7bf05f75
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-05 09:39:32 +0700

    dialogs html1

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -4,16 +4,30 @@ function Messages() {
             <h1 className="dialogs__title">Dialogs</h1>
             <div className="dialogs__container">
                 <ul className="dialogs__list dialogs__list_type_dialog">
-                    <li className="dialogs__item">Geralt of Rivia</li>
-                    <li className="dialogs__item">Cirilla</li>
-                    <li className="dialogs__item">Triss Merigold</li>
-                    <li className="dialogs__item">Keira Metz</li>
-                    <li className="dialogs__item">Philippa Eilhart</li>
-                    <li className="dialogs__item">Sheala de Tancarville</li>
-                    <li className="dialogs__item">Lodge of Sorceresses</li>
+                    <li className="dialogs__item">
+                        <span className="dialogs__text">Geralt of Rivia</span>
+                    </li>
+                    <li className="dialogs__item">
+                        <span className="dialogs__text">Cirilla</span>
+                    </li>
+                    <li className="dialogs__item">
+                        <span className="dialogs__text">Triss Merigold</span>
+                    </li>
+                    <li className="dialogs__item">
+                        <span className="dialogs__text">Keira Metz</span>
+                    </li>
+                    <li className="dialogs__item">
+                        <span className="dialogs__text">Philippa Eilhart</span>
+                    </li>
+                    <li className="dialogs__item">
+                        <span className="dialogs__text">Sheala de Tancarville</span>
+                    </li>
+                    <li className="dialogs__item">
+                        <span className="dialogs__text">Lodge of Sorceresses</span>
+                    </li>
                 </ul>
                 <ul className="dialogs__list dialogs__list_type_messages">
-                    
+
                 </ul>
             </div>
         </section>
