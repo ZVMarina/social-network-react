316dac1e45504691d73f98e02e5c65243eab27bd
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-05 09:22:31 +0700

    dialogs html

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,6 +1,22 @@
 function Messages() {
     return (
-        <div>Messages</div>
+        <section className="dialogs">
+            <h1 className="dialogs__title">Dialogs</h1>
+            <div className="dialogs__container">
+                <ul className="dialogs__list dialogs__list_type_dialog">
+                    <li className="dialogs__item">Geralt of Rivia</li>
+                    <li className="dialogs__item">Cirilla</li>
+                    <li className="dialogs__item">Triss Merigold</li>
+                    <li className="dialogs__item">Keira Metz</li>
+                    <li className="dialogs__item">Philippa Eilhart</li>
+                    <li className="dialogs__item">Sheala de Tancarville</li>
+                    <li className="dialogs__item">Lodge of Sorceresses</li>
+                </ul>
+                <ul className="dialogs__list dialogs__list_type_messages">
+                    
+                </ul>
+            </div>
+        </section>
     )
 }
 
