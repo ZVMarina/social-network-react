84e3881103e41c5576017bf0696d41994d9d6ef0
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-05 10:00:04 +0700

    css dialogs

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -2,34 +2,34 @@ function Messages() {
     return (
         <section className="dialogs">
             <h1 className="dialogs__title">Dialogs</h1>
-            <div className="dialogs__container">
                 <ul className="dialogs__list dialogs__list_type_dialog">
-                    <li className="dialogs__item">
+                    <li className="dialogs__item dialogs__item_type_dialog">
                         <span className="dialogs__text">Geralt of Rivia</span>
                     </li>
-                    <li className="dialogs__item">
+                    <li className="dialogs__item dialogs__item_type_dialog">
                         <span className="dialogs__text">Cirilla</span>
                     </li>
-                    <li className="dialogs__item">
+                    <li className="dialogs__item dialogs__item_type_dialog">
                         <span className="dialogs__text">Triss Merigold</span>
                     </li>
-                    <li className="dialogs__item">
+                    <li className="dialogs__item dialogs__item_type_dialog">
                         <span className="dialogs__text">Keira Metz</span>
                     </li>
-                    <li className="dialogs__item">
+                    <li className="dialogs__item dialogs__item_type_dialog">
                         <span className="dialogs__text">Philippa Eilhart</span>
                     </li>
-                    <li className="dialogs__item">
+                    <li className="dialogs__item dialogs__item_type_dialog">
                         <span className="dialogs__text">Sheala de Tancarville</span>
                     </li>
-                    <li className="dialogs__item">
+                    <li className="dialogs__item dialogs__item_type_dialog">
                         <span className="dialogs__text">Lodge of Sorceresses</span>
                     </li>
                 </ul>
                 <ul className="dialogs__list dialogs__list_type_messages">
-
+                    <li className="dialogs__item dialogs__item_type_messages">Hey, Yen! Where is our unicorn?</li>
+                    <li className="dialogs__item dialogs__item_type_messages">Yen, Geralt and I are practicing sword strikes. See you later.</li>
+                    <li className="dialogs__item dialogs__item_type_messages">When are you coming to the meeting, Yen?</li>
                 </ul>
-            </div>
         </section>
     )
 }
