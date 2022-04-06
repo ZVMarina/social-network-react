087cec553e43ecd11a41198f2655c15a52dc1674
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-06 21:40:35 +0700

    refactor2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,23 +2,26 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav className="navigation">
-            <li className="navigation__item">
-                <NavLink className="navigation__link" to="/profile">Profile</NavLink>
-            </li>
-            <li className="navigation__item">
-                <NavLink className="navigation__link" to="/dialogs">Messages</NavLink>
-            </li>
-            <li className="navigation__item">
-                <NavLink className="navigation__link" to="/news" >News</NavLink>
-            </li>
-            <li className="navigation__item">
-                <NavLink className="navigation__link" to="/music">Music</NavLink>
-            </li>
-            <li className="navigation__item">
-                <NavLink className="navigation__link" to="/settings">Settings</NavLink>
-            </li>
-        </nav>
+        <>
+            <nav className="navigation">
+                <li className="navigation__item">
+                    <NavLink className="navigation__link" to="/profile">Profile</NavLink>
+                </li>
+                <li className="navigation__item">
+                    <NavLink className="navigation__link" to="/dialogs">Messages</NavLink>
+                </li>
+                <li className="navigation__item">
+                    <NavLink className="navigation__link" to="/news" >News</NavLink>
+                </li>
+                <li className="navigation__item">
+                    <NavLink className="navigation__link" to="/music">Music</NavLink>
+                </li>
+                <li className="navigation__item">
+                    <NavLink className="navigation__link" to="/settings">Settings</NavLink>
+                </li>
+            </nav>
+        </>
+
     )
 }
 
