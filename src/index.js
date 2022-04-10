0454874cfff5d5b91584249074878d473b114977
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-10 10:37:14 +0700

    refactor naiming

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals'
-import store from './redax/state';
+import store from './redax/store';
 
 const rerenderTree = () => {
   ReactDOM.render(
