ffc3ffc0afe052f3227e8da309ce34bb1333c1d1
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-11 20:45:47 +0700

    refactor: main image

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,7 +7,7 @@ function Profile(props) {
     return (
         <>
             <section className="profile">
-                <img className="profile__main-image" src={image} />
+                <img className="profile__main-image" src='https://hb.bizmrg.com/cybersportru-media/97/973b06a7d96a83ddaa859ef024c38b7d.jpg' />
                 <ProfileInfo
                     url="https://games.mail.ru/hotbox/content_files/news/2021/06/29/150b75b87bf64e07a2465f34aa08d7e7.jpg"
                     name="Yennefer of Vengerberg"
