a811f2d2f2527822c44f357a758f4bc7c2596b3c
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-11 10:26:01 +0700

    refactor: index.js delete rerenderTree

--- src/index.js
+++ src/index.js
@@ -5,24 +5,16 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals'
 import store from './redux/reduxStore';
 
-const rerenderTree = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App
-        store={store}
-        state={store.getState()}
-        dispatch={store.dispatch.bind(store)}
-      />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+ReactDOM.render(
+  <React.StrictMode>
+    <App
+      store={store}
+      state={store.getState()}
+      dispatch={store.dispatch.bind(store)}
+    />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-  reportWebVitals();
-}
-
-rerenderTree(store.getState());
-
-store.subscribe(() => {
-  rerenderTree(store.getState())
-});
+reportWebVitals();
 
