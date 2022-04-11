8d5ce048d50334b5324030188a3fb0af54bf303d
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-11 19:42:23 +0700

    refactor: array users move to Users (wrong)

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -4,10 +4,22 @@ const setUsersActionType = 'set-users';
 
 const initialState = {
     users: [
-        { id: 1, photoUrl: 'https://www.meme-arsenal.com/memes/97ed5c47c17b0f5b515f53c4036e590c.jpg', followed: true, name: "Geralt of Rivia", status: "I'm killing monsters", country: "Rivia" },
-        { id: 2, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/1811900/pub_5d937d6ae3062c00b016bbd5_5d94144598fe7900b14fc924/scale_1200', followed: true, name: "Cirilla", status: "I'm killing monsters with Geralt", country: "Cintra" },
-        { id: 3, photoUrl: 'https://zikurat.media/wp-content/uploads/2020/05/Gd3OP1l3_o.jpg', followed: false, name: "Triss Merigold", status: "I want to be Geralt's woman", country: "Temeria" },
-        { id: 4, photoUrl: 'https://sun1-14.userapi.com/s/v1/if1/4Q2tBt2Vs0D1iR2fdfYyNtK8Y5EKQSpe2FLD5IlPpuzZ9hv26bZRsBr5afcI_1c33OzNS1Yu.jpg?size=400x533&quality=96&crop=0,0,453,604&ava=1', followed: false, name: "Zoltan Chivay", status: "It's a dog's life Geralt, I'll tell you that much", country: "Mahakam" },
+        /* {
+            id: 1, photoUrl: 'https://www.meme-arsenal.com/memes/97ed5c47c17b0f5b515f53c4036e590c.jpg',
+            followed: true, name: "Geralt of Rivia", status: "I'm killing monsters", country: "Rivia"
+        },
+        {
+            id: 2, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/1811900/pub_5d937d6ae3062c00b016bbd5_5d94144598fe7900b14fc924/scale_1200',
+            followed: true, name: "Cirilla", status: "I'm killing monsters with Geralt", country: "Cintra"
+        },
+        {
+            id: 3, photoUrl: 'https://zikurat.media/wp-content/uploads/2020/05/Gd3OP1l3_o.jpg',
+            followed: false, name: "Triss Merigold", status: "I want to be Geralt's woman", country: "Temeria"
+        },
+        {
+            id: 4, photoUrl: 'https://sun1-14.userapi.com/s/v1/if1/4Q2tBt2Vs0D1iR2fdfYyNtK8Y5EKQSpe2FLD5IlPpuzZ9hv26bZRsBr5afcI_1c33OzNS1Yu.jpg?size=400x533&quality=96&crop=0,0,453,604&ava=1',
+            followed: false, name: "Zoltan Chivay", status: "It's a dog's life Geralt, I'll tell you that much", country: "Mahakam"
+        }, */
     ]
 }
 
@@ -41,7 +53,7 @@ const usersReducer = (state = initialState, action) => {
             return {
                 ...state,
                 // сначала сделал копию юзеров, затем добавили туда пришедших юзеров
-                users: [...state.users, ...action.users]
+                users: [/* ...state.users,  */...action.users]
             }
 
         default: return state;
