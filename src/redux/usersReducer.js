9e5db510c22b6281e7bcc08500bd27c2388f3c00
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-13 19:41:18 +0700

    refactor: clear

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -5,24 +5,7 @@ const setPageActionType = 'set-page';
 const setTotalUsersCountActionType = 'set-total-users-count';
 
 const initialState = {
-    users: [
-        /* {
-            id: 1, photoUrl: 'https://www.meme-arsenal.com/memes/97ed5c47c17b0f5b515f53c4036e590c.jpg',
-            followed: true, name: "Geralt of Rivia", status: "I'm killing monsters", country: "Rivia"
-        },
-        {
-            id: 2, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/1811900/pub_5d937d6ae3062c00b016bbd5_5d94144598fe7900b14fc924/scale_1200',
-            followed: true, name: "Cirilla", status: "I'm killing monsters with Geralt", country: "Cintra"
-        },
-        {
-            id: 3, photoUrl: 'https://zikurat.media/wp-content/uploads/2020/05/Gd3OP1l3_o.jpg',
-            followed: false, name: "Triss Merigold", status: "I want to be Geralt's woman", country: "Temeria"
-        },
-        {
-            id: 4, photoUrl: 'https://www.xgamers.ru/Handler1.ashx?id=4363',
-            followed: false, name: "Zoltan Chivay", status: "It's a dog's life Geralt, I'll tell you that much", country: "Mahakam"
-        }, */
-    ],
+    users: [],
     pageSize: 25,
     totalUsersCount: 0,
     currentPage: 1,
