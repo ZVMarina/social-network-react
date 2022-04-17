e09d91b4cca4c0453c4582f6dc4b52e89141b831
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-17 10:55:46 +0700

    refactor: api baseUrl

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,9 @@
 import * as axios from 'axios';
 
+const baseUrl = `https://social-network.samuraijs.com/api/1.0/`
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
+    return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {
                 withCredentials: true
             })
             .then(response => response.data)
