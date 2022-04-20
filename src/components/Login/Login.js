e53c1939be57f06e7f90a4b5c46a53a04b5d2331
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-20 13:01:57 +0700

    form css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,19 @@ import React from "react"
 
 function Login() {
     return (
-        <div>Login</div>
+        <section className="login">
+            <h1 className="title">Login</h1>
+            <form className="form">
+                <input className="form__input" placeholder={"Login"} required />
+                <input className="form__input" placeholder={"Pasword"} required />
+                <div className="form__checkbox-container">
+                    <label className="form__label" for="remember-me">Запомнить меня</label>
+                    <input className="form__checkbox" id="remember-me" type={"checkbox"} />
+                </div>
+                <button className="button form__button">Login</button>
+            </form>
+        </section>
+
     )
 }
 
