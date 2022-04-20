4f89c5f1326be0cb2c590d982abd0c83fb5c220b
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-20 15:13:53 +0700

    refactor form validate

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,10 +6,13 @@ const validateLoginForm = values => {
     const errors = {};
     if (!values.email) {
         errors.email = 'Required';
-    } else if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-    ) {
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
         errors.email = 'Invalid email address';
+    }
+    if (!values.password) {
+        errors.password = 'Required';
+    } else if (values.password.length < 6) {
+        errors.password = 'Must be longer than 6 characters';
     }
     return errors;
 };
@@ -21,7 +24,6 @@ const Login = () => {
             <Formik
                 initialValues={{ email: "", password: "", rememberMe: false }}
                 validate={validateLoginForm}
-                validationSchema={loginFormSchema}
                 onSubmit={(values) => {
                     console.log(values);
                 }}
