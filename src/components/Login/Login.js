a90a208ee1a0709c92705b69072d4f904c06c541
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-20 13:13:18 +0700

    LoginForm component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,26 @@
 import React from "react"
 
-function Login() {
+const Login = (props) => {
     return (
         <section className="login">
             <h1 className="title">Login</h1>
-            <form className="form">
-                <input className="form__input" placeholder={"Login"} required />
-                <input className="form__input" placeholder={"Pasword"} required />
-                <div className="form__checkbox-container">
-                    <label className="form__label" for="remember-me">Запомнить меня</label>
-                    <input className="form__checkbox" id="remember-me" type={"checkbox"} />
-                </div>
-                <button className="button form__button">Login</button>
-            </form>
+            <LoginForm />
         </section>
 
+    )
+}
+
+const LoginForm = (props) => {
+    return (
+        <form className="form">
+            <input className="form__input" placeholder={"Login"} required />
+            <input className="form__input" placeholder={"Pasword"} required />
+            <div className="form__checkbox-container">
+                <label className="form__label" for="remember-me">Запомнить меня</label>
+                <input className="form__checkbox" id="remember-me" type={"checkbox"} />
+            </div>
+            <button className="button form__button">Login</button>
+        </form>
     )
 }
 
