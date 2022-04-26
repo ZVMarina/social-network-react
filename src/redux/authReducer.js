5de8d7840c2d9534e5ce39a89e0f567c885c931c
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-26 20:49:23 +0700

    refactor: replaced  then with async await i authReducer

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -24,7 +24,7 @@ const authReducer = (state = initialState, action) => {
 export const setAuthUserDataAC = (id, email, login, isAuth) => ({ type: SET_AUTH_DATA_ACTION_TYPE, data: { id, email, login, isAuth } })
 
 export const getAuthInfoThunkCreator = () => async (dispatch) => {
-    let data = await authApi.getAuthInfo()
+    const data = await authApi.getAuthInfo()
 
             if (data.resultCode === 0) {
                 const authInfo = data.data
