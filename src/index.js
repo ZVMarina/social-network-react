70731edc69f3ad823002d5e2cddde7ec98722638
ZVMarina <zvyagintsevame@yandex.ru>
2022-04-28 14:34:10 +0700

    refactor: HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './index.css';
 import App from './components/App';
 import store from './redux/reduxStore';
-import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import * as ReactDOMClient from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
